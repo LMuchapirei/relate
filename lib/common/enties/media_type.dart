509181f118938e7ee63efac3d7e4049b1a2b2fe6
LMuchapirei <muchapireil@gmail.com>
2024-11-26 11:11:15 +0200

    refactor

--- lib/common/enties/media_type.dart
+++ lib/common/enties/media_type.dart
@@ -1,4 +1,3 @@
-import 'dart:typed_data';
 import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
